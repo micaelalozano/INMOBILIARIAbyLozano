4ee134f9e0263ed1fdf19da3f31d91f5f1f802fc
micaelalozano <micaelalozano.16@hotmail.com>
2023-02-26 17:40:20 -0300

    New Commit

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -6,7 +6,18 @@ const HeroImage = () => {
   return (
     <>
       <div className="contenedor-img">
-        <div className="item-img">HOLA</div>
+        <div className="content">
+          <div className="visible">
+            <p className="p">Hola!</p>
+            <ul className="ul">
+              <li className="it">
+                <strong>Bienvenido</strong>{" "}
+              </li>
+              <li className="it">usuario</li>
+            </ul>
+          </div>
+        </div>
+        <div className="aca"><img src="https://media.admagazine.com/photos/61e5acc706c10ae95c71b902/16:9/w_2560%2Cc_limit/New-York-skyline.jpg" alt="" /></div>
       </div>
     </>
   );
